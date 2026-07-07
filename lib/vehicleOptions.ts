d8f09344a8af1aa13c3cc03de51cf0fc97d39b2e
Autofy Deploy <autofy@autofy.dev>
2026-07-07 08:04:29 +0000

    Engine option: 'Electric (no engine)' -> 'Electric (maafi engine)'

--- lib/vehicleOptions.ts
+++ lib/vehicleOptions.ts
@@ -1,7 +1,7 @@
 export const SPECIFICATIONS = ['GCC', 'American', 'Japanese', 'Korean', 'European', 'Canadian', 'Chinese', 'Other'];
 export const DRIVETRAINS = ['FWD', 'RWD', 'AWD', '4WD'];
 export const FUEL_TYPES = ['Petrol', 'Diesel', 'Hybrid', 'Plug-in Hybrid', 'Electric'];
-export const ENGINES = ['I3', 'I4', 'I5', 'I6', 'V6', 'V8', 'V10', 'V12', 'V16', 'W16', 'Electric (no engine)'];
+export const ENGINES = ['I3', 'I4', 'I5', 'I6', 'V6', 'V8', 'V10', 'V12', 'V16', 'W16', 'Electric (maafi engine)'];
 export const TRANSMISSIONS = ['Automatic', 'Manual'];
 export const SEAT_OPTIONS = [2, 3, 4, 5, 6];
 export const EXTERIOR_COLORS = ['White', 'Black', 'Silver', 'Gray', 'Red', 'Blue', 'Green', 'Yellow', 'Gold', 'Beige', 'Brown', 'Orange', 'Bronze', 'Pink', 'Other'];
